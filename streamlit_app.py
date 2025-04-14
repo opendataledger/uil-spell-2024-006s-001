99d121f4cec912d608e5f002bcbbc338644e611e
opendataledger <opendataledger@gmail.com>
2025-04-14 15:51:23 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from time import sleep
+from navigation import make_sidebar, check_user_inactivity  # Import necessary functions
+
+# Check for inactivity and logout if necessary
+check_user_inactivity()
+
+# Add sidebar
+make_sidebar()
+
+st.title("Welcome to Cheatham Speller β wip 6S +Test")
+
+# Inject JavaScript to focus the input field on the page load globally
+st.components.v1.html("""<script>
+    const userInput = document.getElementById('user_input');
+    if (userInput) {
+        userInput.focus();
+    }
+</script>""", height=0)
+
+st.write("🔒 Please log in to continue.")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Log in", type="primary"):
+    if username == "uiltest" and password == "cheathamtest":
+        st.session_state.logged_in = True
+        st.success("✔️Logged in successfully!")
+        sleep(0.5)
+        
+        # Instead of using st.switch_page(), use st.experimental_rerun() to reload the page.
+        st.switch_page("pages/page1.py") # Store the next page in session state
+        # st.experimental_rerun()  # Trigger a rerun to go to the new page
+    else:
+        st.error("❌Incorrect username or password")
