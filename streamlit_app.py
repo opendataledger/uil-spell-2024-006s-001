0a8f510e1b1b59081a6f0e354db87386eb0c0660
opendataledger <opendataledger@gmail.com>
2025-04-15 17:05:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,15 @@ check_user_inactivity()
 # Add sidebar
 make_sidebar()
 
-st.title("Welcome to Cheatham Speller β wip 6S +Test")
+st.title("Welcome to Curtis Speller ββ wip 6S +Test")
 
 # Inject JavaScript to focus the input field on the page load globally
-st.components.v1.html("""
-    <script>
-        // Check if the element with id 'user_input' exists and set focus
-        const userInput = document.getElementById('user_input');
-        if (userInput) {
-            userInput.focus();
-        }
-    </script>
-""", height=0)
+st.components.v1.html("""<script>
+    const userInput = document.getElementById('user_input');
+    if (userInput) {
+        userInput.focus();
+    }
+</script>""", height=0)
 
 st.write("🔒 Please log in to continue.")
 
@@ -27,10 +24,13 @@ username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if username == "uiltest" and password == "cheathamtest":
+    if username == "uiltest" and password == "curtistest":
         st.session_state.logged_in = True
         st.success("✔️Logged in successfully!")
         sleep(0.5)
-        st.switch_page("pages/page1.py")
+        
+        # Instead of using st.switch_page(), use st.experimental_rerun() to reload the page.
+        st.switch_page("pages/page1.py") # Store the next page in session state
+        # st.experimental_rerun()  # Trigger a rerun to go to the new page
     else:
         st.error("❌Incorrect username or password")
